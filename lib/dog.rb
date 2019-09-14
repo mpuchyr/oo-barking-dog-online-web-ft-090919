becbfa86d481d365acdc7d305926b14ab6ab7f1c
Matt Puchyr <matt.puchyr@gmail.com>
2019-09-14 02:27:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Dog
+
+  def name=(dog_name)
+    @dogs_name = dog_name
+  end
+  
+  def name
+    @dogs_name
+  end
+  
+  def bark
+    puts "Woof!"
+  end
+  
+end
